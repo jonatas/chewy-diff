c2af10e90a7075d003c99f2d8774f7267909e2a9
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2017-07-06 22:32:42 -0300

    Implement `Diff.changes` with the primary IndexAnalyzer supporting fields and witchcrafts :tada:

--- lib/chewy/diff.rb
+++ lib/chewy/diff.rb
@@ -1,7 +1,71 @@
 require "chewy/diff/version"
+require 'parser'
+require 'parser/current'
+require '../../fast/lib/fast'
+require 'pry'
 
 module Chewy
   module Diff
-    # Your code goes here...
+    def self.changes(before, after)
+      return [] if before == after
+      ast_before = ast(before)
+      ast_after = ast(after)
+      return [] if ast_before == ast_after
+
+      from = IndexAnalyzer.new(ast_before)
+      to = IndexAnalyzer.new(ast_after)
+
+      diff(from, to)
+    end
+
+    def self.diff(from, to)
+      removed = from.fields - to.fields
+      added =  to.fields - from.fields
+#puts "from",from.fields,"to", to.fields
+#puts "added", added.inspect, "removed", removed.inspect
+      {}.tap do |diff|
+        diff[:-] = "#{from.index_name}#{describe(removed)}" if removed.any?
+        diff[:+] = "#{from.index_name}#{describe(added)}" if added.any?
+      end
+    end
+
+    def self.describe fields
+      fields.map(&method(:field_name)).flatten
+    end
+
+    def self.field_name(field)
+      if field.children.size > 2
+        field.children[2].children[0]
+      else
+        field.children[1]
+      end
+    end
+
+    class IndexAnalyzer
+      def initialize ast
+        @index_name, @fields =
+          Fast.search '(block (send nil define_type $...) ... $...)', ast
+      end
+
+      def fields
+         @fields.children.select do |field|
+           %i[field field_with_crutch witchcraft!].include?(field.children[1])
+         end
+      end
+
+      def witchcrafts
+require 'pry'; binding.pry
+        Fast.search('(send nil witchcraft!)', @fields).flatten
+      end
+
+      def index_name
+        @index_name.children.last
+      end
+    end
+
+    def self.ast(code)
+      Parser::CurrentRuby.parse(code)
+    end
+
   end
 end
--- spec/chewy/diff_spec.rb
+++ spec/chewy/diff_spec.rb
@@ -1,11 +1,117 @@
 require "spec_helper"
 
 RSpec.describe Chewy::Diff do
-  it "has a version number" do
-    expect(Chewy::Diff::VERSION).not_to be nil
-  end
+  let(:index_before) { <<~RUBY }
+    class CitiesIndex < Chewy::Index
+      define_type City do
+        field :name, value: -> { name.strip }
+        field :popularity
+      end
+    end
+  RUBY
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context ".changes" do
+
+    subject { Chewy::Diff.changes(index_before, index_after) }
+
+    context 'equal files should return no difference' do
+      let(:index_after) { index_before }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'changed order but not the implementation' do
+      let(:index_after) { <<~RUBY }
+        class CitiesIndex < Chewy::Index
+          define_type City do
+            field :popularity
+            field :name, value: -> { name.strip }
+          end
+        end
+      RUBY
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'ignore preload changes' do
+      let(:index_after) { <<~RUBY }
+        class CitiesIndex < Chewy::Index
+          define_type City.preload(:state) do
+            field :name, value: -> { name.strip }
+            field :popularity
+          end
+        end
+      RUBY
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'add witchcraft!' do
+      let(:index_after) { <<~RUBY }
+        class CitiesIndex < Chewy::Index
+          define_type City do
+            field :name, value: -> { name.strip }
+            field :popularity
+            witchcraft!
+          end
+        end
+      RUBY
+
+      it { is_expected.to eq({ :+ => "City[:witchcraft!]" })}
+    end
+
+
+    context 'changed field implementation' do
+      let(:index_after) { <<~RUBY }
+        class CitiesIndex < Chewy::Index
+          define_type City do
+            field :popularity
+            field :name, value: -> { name.uppercase }
+          end
+        end
+      RUBY
+
+      specify do
+        is_expected.to eq({ :- => "City[:name]", :+ => "City[:name]" })
+      end
+    end
+
+    context 'added new field implementation' do
+      let(:index_after) { <<~RUBY }
+        class CitiesIndex < Chewy::Index
+          define_type City do
+            field :name, value: -> { name.strip }
+            field :latitude
+            field :longitude
+          end
+        end
+      RUBY
+
+      specify do
+        is_expected.to eq({
+           :- => "City[:popularity]",
+           :+ => "City[:latitude, :longitude]"
+        })
+      end
+    end
+
+    context 'support crutches' do
+      let(:index_after) { <<~RUBY }
+        class CitiesIndex < Chewy::Index
+          define_type City do
+            field :name, value: -> { name.strip }
+            field_with_crutch :location, :talents, type: 'geo_point'
+          end
+        end
+      RUBY
+
+      specify do
+        is_expected.to eq({
+           :- => "City[:popularity]",
+           :+ => "City[:location]"
+        })
+      end
+    end
+
   end
 end
